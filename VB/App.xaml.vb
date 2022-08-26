0bf85c42861cce980ef011de17b04fe8b74ab15a
DevExpressExampleBot <fakeEmail>
2022-08-26 16:51:56 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,14 @@
+Imports System.Windows
+
+Namespace DXGridSample
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+        Public Sub New()
+        End Sub
+    End Class
+End Namespace
--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,85 +1,93 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Collections.ObjectModel
 Imports DevExpress.Mvvm
 Imports System.Windows.Media
 Imports DevExpress.Xpf.Grid
 
 Namespace DXGridSample
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
-	Public Class ViewModel
-		Inherits BindableBase
+    Public Partial Class MainWindow
+        Inherits System.Windows.Window
 
-		Public Property Countries() As ObservableCollection(Of String)
-			Get
-				Return GetValue(Of ObservableCollection(Of String))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of String))
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Items() As ObservableCollection(Of Item)
-			Get
-				Return GetValue(Of ObservableCollection(Of Item))()
-			End Get
-			Set(ByVal value As ObservableCollection(Of Item))
-				SetValue(value)
-			End Set
-		End Property
-		Public Property HighlightVisited() As Boolean
-			Get
-				Return GetValue(Of Boolean)()
-			End Get
-			Set(ByVal value As Boolean)
-				SetValue(value)
-			End Set
-		End Property
-		Public Sub New()
-			Countries = New ObservableCollection(Of String) From {"USA", "Germany", "Russia"}
-			Items = New ObservableCollection(Of Item)()
-			Dim i As Integer = 0
-			For Each country In Countries
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: Items.Add(new Item { Country = country, Visits = i++ });
-				Items.Add(New Item With {
-					.Country = country,
-					.Visits = i
-				})
-				i += 1
-			Next country
-		End Sub
-	End Class
-	Public Class Item
-		Inherits BindableBase
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-		Public Property Country() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Visits() As Integer
-			Get
-				Return GetValue(Of Integer)()
-			End Get
-			Set(ByVal value As Integer)
-				SetValue(value)
-			End Set
-		End Property
-		Public Property Color() As Color
-			Get
-				Return GetValue(Of Color)()
-			End Get
-			Set(ByVal value As Color)
-				SetValue(value)
-			End Set
-		End Property
-	End Class
+    Public Class ViewModel
+        Inherits DevExpress.Mvvm.BindableBase
+
+        Public Property Countries As ObservableCollection(Of String)
+            Get
+                Return GetValue(Of System.Collections.ObjectModel.ObservableCollection(Of String))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of String))
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Items As ObservableCollection(Of DXGridSample.Item)
+            Get
+                Return GetValue(Of System.Collections.ObjectModel.ObservableCollection(Of DXGridSample.Item))()
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of DXGridSample.Item))
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property HighlightVisited As Boolean
+            Get
+                Return GetValue(Of Boolean)()
+            End Get
+
+            Set(ByVal value As Boolean)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Sub New()
+            Me.Countries = New System.Collections.ObjectModel.ObservableCollection(Of String) From {"USA", "Germany", "Russia"}
+            Me.Items = New System.Collections.ObjectModel.ObservableCollection(Of DXGridSample.Item)()
+            Dim i As Integer = 0
+            For Each country In Me.Countries
+                Me.Items.Add(New DXGridSample.Item With {.Country = country, .Visits = System.Math.Min(System.Threading.Interlocked.Increment(i), i - 1)})
+            Next
+        End Sub
+    End Class
+
+    Public Class Item
+        Inherits DevExpress.Mvvm.BindableBase
+
+        Public Property Country As String
+            Get
+                Return GetValue(Of String)()
+            End Get
+
+            Set(ByVal value As String)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Visits As Integer
+            Get
+                Return GetValue(Of Integer)()
+            End Get
+
+            Set(ByVal value As Integer)
+                SetValue(value)
+            End Set
+        End Property
+
+        Public Property Color As Color
+            Get
+                Return GetValue(Of System.Windows.Media.Color)()
+            End Get
+
+            Set(ByVal value As Color)
+                SetValue(value)
+            End Set
+        End Property
+    End Class
 End Namespace
